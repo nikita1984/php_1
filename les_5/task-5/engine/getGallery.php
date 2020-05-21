9c85d1ac4f2c839e7834f4689ed7cc863e7ec16b
nikita1984 <52231270+nikita1984@users.noreply.github.com>
2020-05-21 21:34:51 +0300

    реализовано построение галереи на основе данных полученных из SQL-запроса к БД (часть 2)

--- les_5/task-5/engine/getGallery.php
+++ les_5/task-5/engine/getGallery.php
@@ -1,34 +1,15 @@
 <?php
-/** Возвращает массив с путем файлов галереи для вставки в HTML-код
- * @param $directoryPath - путь к папке с файлами изображений блока галереи
- * @return array массив с путем файлов галереи для вставки в HTML-код
+/** Возвращает массив с параметрами галереи для вставки в HTML-код
+ * @param $host - адрес хоста на котором расположена БД
+ * @param $login - логин, под которым заходим в БД
+ * @param  $pwd - пароль к логину, под которым заходим в БД
+ * @param $db - наименование БД к которой будет происходить SQL-запрос
+ * @param $sql - SQL-запрос
+ * @return array массив с параметрами галереи для вставки в HTML-код
  */
-//function getGallery (string $directoryPath) : array {
-//    $directoryList = [];
-//    foreach (scandir($directoryPath) as $item) {
-//        if (!is_dir($item)){
-//            $file = [
-//                "small" => getFilePath($item, 'small'),
-//                "big" => getFilePath($item, 'big'),
-//                "alt" => "$item",
-//                "href" => str_replace('jpg', 'php', $item)
-//            ];
-//            $fileSizes = DOWNLOAD_FILE_SIZE;
-//            if (isJPG($file["big"]) && lessThenSize($file["big"], $fileSizes)) {
-//                array_push($directoryList, $file);
-//            }
-//        }
-//    };
-//    return $directoryList;
-//};
+function getGallery (string $host, string $login, string $pwd, string $db, string $sql) {
 
-function getGallery () {
-    $host = '127.0.0.1:3306';
-    $user = 'admin';
-    $pwd = 'admin';
-    $db = 'gallery';
-
-    $link = mysqli_connect($host, $user, $pwd, $db);
+    $link = mysqli_connect($host, $login, $pwd, $db);
     if (!$link) {
         echo "Ошибка: Невозможно установить соединение с MySQL." . PHP_EOL;
         echo "Код ошибки errno: " . mysqli_connect_errno() . PHP_EOL;
@@ -36,16 +17,13 @@ function getGallery () {
         exit;
     }
 
-    $sql = 'SELECT * from gallerytable';
     $result = mysqli_query($link, $sql);
     if (!$result){
         var_dump(mysqli_error($link));
         exit;
     }
 
-
     $final = mysqli_fetch_all($result, MYSQLI_ASSOC);
-    // var_dump($final);
     mysqli_close($link);
     return $final;
 }
--- les_5/task-5/templates/gallery.php
+++ les_5/task-5/templates/gallery.php
@@ -1,13 +1,15 @@
 <?php
 $host = '127.0.0.1:3306';
-$user = 'admin';
+$SQLLogin = 'admin';
 $pwd = 'admin';
 $db = 'gallery';
-$gallery = getGallery();
-// $gallery = getSQLGallery();
+$sql = 'SELECT * from gallerytable';
+$gallery = getGallery($host, $SQLLogin, $pwd, $db, $sql);
 ?>
 <div class="gallery">
     <? foreach ($gallery as $key => $item): ?>
-        <a href="<?=$item["href"]?>" target="_blank"><img class="miniIMG" src="<?=$item['smallImagePath']?>" alt="<?=$item["name"]?>"></a>
+        <a href="<?=$item["href"]?>" target="_blank">
+            <img class="miniIMG" src="<?=$item["smallImagePath"]?>" alt="<?=$item["name"]?>">
+        </a>
     <? endforeach; ?>
 </div>
