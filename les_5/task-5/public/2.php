5db252d73fdbb25e7eb303555b7d7eb088bde9f3
nikita1984 <52231270+nikita1984@users.noreply.github.com>
2020-05-21 21:06:35 +0300

    реализовано отображение просмотра конкретной фотографии на отдельной странице (часть 3 финал)

---
+++ les_5/task-5/public/2.php
@@ -0,0 +1,14 @@
+<?php
+// Устанавливаем "строгий" режим
+declare(strict_types = 1);
+// Подключаем файл с константами и настройками конфигурации
+require __DIR__ . '\..\config\main.php';
+
+// Подключаем файлы с функциями
+requireFunctions(scandir(ENGINE_DIR));
+
+// Отрисовываем страницу
+echo renderImage("2.jpg");
+
+
+
