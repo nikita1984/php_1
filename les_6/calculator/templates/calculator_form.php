70db6e304e559f1f2aa35880b79f87a22d0df485
nikita1984 <52231270+nikita1984@users.noreply.github.com>
2020-05-26 22:27:25 +0300

    task 6-1, 6-2 completed

--- les_6/calculator/templates/calculator_form.php
+++ les_6/calculator/templates/calculator_form.php
@@ -1,13 +1,16 @@
 <?php
 $form_title = "Калькулятор";
-// $result = "";
+$operationTitle = "Введи аргументы и выбери математическую операцию";
 ?>
 <h2 class="flexCenter"><?= $form_title ?></h2>
 <form class="flexCenter" action="" enctype="multipart/form-data" method="post">
+
     <div class="formDiv">
-        <p>Аргумент 1: <input type="number" class="formInput" name='arg1' value="2"></p>
-        <p>Аргумент 2: <input type="number" class="formInput" name='arg2' value="1"></p>
+        <h4 class="flexCenter"><?= $operationTitle ?></h4>
+        <p>Аргумент 1: <input type="number" class="formInput" name='arg1' value="0"></p>
+        <p>Аргумент 2: <input type="number" class="formInput" name='arg2' value="0"></p>
     </div>
+
     <div class="formDiv">
         <input type="submit" name='operation' value="summ">
         <input type="submit" name='operation' value="difference">
