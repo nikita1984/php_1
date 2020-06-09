31797e965b22a36a85e478f8198775a5b7895fc6
nikita1984 <52231270+nikita1984@users.noreply.github.com>
2020-06-10 01:56:32 +0300

    tasks 6-4: оптимизация рендеринга каталога и страницы товара (часть 4 финал)

--- les_6/catalog/public/index.php
+++ les_6/catalog/public/index.php
@@ -9,24 +9,11 @@ if($_GET['page'] == 'singlePage') {
     $contentArray = [
         'title' => 'singlePage',
         'template' => 'singlePage_main',
-        'params' => [
-            'imgPanel' => renderTemplate(CONTENTS_DIR . 'singlePage_imgPanel'),
-            'description' => renderTemplate(CONTENTS_DIR . 'singlePage_description'),
-            'likeAlso' => renderTemplate(CONTENTS_DIR . 'singlePage_likeAlso'),
-        ]
     ];
 } else {
     $contentArray = [
         'title' => 'catalog',
         'template' => 'catalog_content',
-        'params' => [
-            'leftSide' => renderTemplate(CONTENTS_DIR . 'catalog_leftSide'),
-            'topFilter' => renderTemplate(CONTENTS_DIR .'catalog_topFilter'),
-            'sort' => renderTemplate(CONTENTS_DIR . 'catalog_sort'),
-            'catalog' => renderTemplate(CONTENTS_DIR . 'catalog_productCatalog'),
-            'catalogBottom' => renderTemplate(CONTENTS_DIR . 'catalog_catalogBottom'),
-            'featureBox' => renderTemplate(CONTENTS_DIR . 'catalog_featureBox')
-        ]
     ];
 }
 
@@ -35,7 +22,7 @@ echo renderTemplate(LAYOUTS_DIR . 'main', [
     'header' => renderTemplate(TEMPLATES_DIR . 'header'),
     'navigation' => renderTemplate(TEMPLATES_DIR . 'navigation'),
     'breadcrumbs' => renderTemplate(TEMPLATES_DIR . 'breadcrumbs'),
-    'content' =>  renderTemplate(CONTENTS_DIR . $contentArray['template'], $contentArray['params']),
+    'content' =>  renderTemplate(CONTENTS_DIR . $contentArray['template']),
     'subscribePanel' => renderTemplate(TEMPLATES_DIR . 'subscribePanel'),
     'footer' => renderTemplate(TEMPLATES_DIR . 'footer'),
     'footerSocial' => renderTemplate(TEMPLATES_DIR . 'footerSocial')
--- les_6/catalog/templates/content_templates/singlePage_main.php
+++ les_6/catalog/templates/content_templates/singlePage_main.php
@@ -1,5 +1,10 @@
+<?php
+$imgPanel = renderTemplate(CONTENTS_DIR . 'singlePage_imgPanel');
+$description = renderTemplate(CONTENTS_DIR . 'singlePage_description');
+$likeAlso = renderTemplate(CONTENTS_DIR . 'singlePage_likeAlso');
+?>
 <div class="top">
-    <?=$imgPanel?>
-    <?=$description?>
-    <?=$likeAlso?>
+    <?= $imgPanel ?>
+    <?= $description ?>
+    <?= $likeAlso ?>
 </div>
