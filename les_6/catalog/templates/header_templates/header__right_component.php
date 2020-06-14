e8f84e0b3467e0d261ce87d79580e8c469f55104
nikita1984 <52231270+nikita1984@users.noreply.github.com>
2020-06-15 00:39:58 +0300

    tasks 7-1: реализация построения корзины через шаблон (подсчёт товара в корзине) (часть_3)

--- les_6/catalog/templates/header_templates/header__right_component.php
+++ les_6/catalog/templates/header_templates/header__right_component.php
@@ -1,13 +1,24 @@
 <?php
 $cartData = [
     0 => [
-        'img' => 'img/sc_img1.jpg',
+        'id' => 1,
         'name' => 'Rebox Zane',
         'rating' => '4',
         'price' => 250,
+        'qty' => 2,
+    ],
+    1 => [
+        'id' => 2,
+        'name' => 'Rebox Zane',
+        'rating' => '3',
+        'price' => 300,
         'qty' => 1,
     ]
-]
+];
+
+foreach ($cartData as $item) {
+    $total += $item['price'] * $item['qty'];
+}
 ?>
 <div class="header__right">
     <div class="sh"><a href="shopping-card.htm"><img src="img/Forma_1.svg" alt="basket"
@@ -15,9 +26,12 @@ $cartData = [
         <div class="sh-count">5</div>
         <div class="shopping-cart__drop">
             <? foreach ($cartData as $cartDataElem): ?>
+                <?php
+                $img = getFilePath($cartDataElem['id'], 'shopping-cart-drop_menu-img');
+                ?>
                 <div class="product-in-sc">
                     <a href="single-page.htm" style="float: left; width: 240px;">
-                        <div class="product-in-sc-img" style="background-image: url(<?= $cartDataElem['img'] ?>)">
+                        <div class="product-in-sc-img" style="background-image: url(<?= $img ?>)">
                         </div>
 
                         <div class="product-in-sc-desc">
@@ -36,29 +50,9 @@ $cartData = [
 
                 </div>
             <? endforeach; ?>
-            <div class="product-in-sc"><a href="single-page.htm" style="float: left; width: 240px;">
-                    <div class="product-in-sc-img" style="background-image: url(img/sc_img2.jpg)">
-                    </div>
-
-                    <div class="product-in-sc-desc">
-                        <h3 class="h3-sc-name">Rebox Zane</h3>
-                        <div class="sc-rating">
-                            <i class="fas fa-star rat"></i>
-                            <i class="fas fa-star rat"></i>
-                            <i class="fas fa-star rat"></i>
-                            <i class="fas fa-star rat"></i>
-                            <i class="fas fa-star rat"></i>
-                        </div>
-                        <div class="sc-count">1&nbsp;x $250</div>
-
-                    </div>
-                </a>
-                <div class="sh__action"><a href="#" class="action"><i
-                            class="far fa-times-circle"></i></a></div>
-            </div>
             <div class="total">
                 <div>total</div>
-                <div>$500.00</div>
+                <div>$<?= $total ?></div>
             </div>
             <a href="checkout.htm" class="sc-btn">Checkout</a>
             <a href="shopping-card.htm" class="sc-btn">go&nbsp;to&nbsp;cart</a>
