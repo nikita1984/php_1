5f914b2df644faec3e3f9ce50af4cfb243ba50b7
nikita1984 <52231270+nikita1984@users.noreply.github.com>
2020-06-14 08:09:57 +0300

    tasks 7-1: реализация построения корзины через шаблон (часть_2)

--- les_6/catalog/templates/header_templates/header__right_component.php
+++ les_6/catalog/templates/header_templates/header__right_component.php
@@ -1,30 +1,41 @@
+<?php
+$cartData = [
+    0 => [
+        'img' => 'img/sc_img1.jpg',
+        'name' => 'Rebox Zane',
+        'rating' => '4',
+        'price' => 250,
+        'qty' => 1,
+    ]
+]
+?>
 <div class="header__right">
     <div class="sh"><a href="shopping-card.htm"><img src="img/Forma_1.svg" alt="basket"
                                                      class="shopping-cart"></a>
         <div class="sh-count">5</div>
         <div class="shopping-cart__drop">
-            <div class="product-in-sc">
-                <a href="single-page.htm" style="float: left; width: 240px;">
-                    <div class="product-in-sc-img" style="background-image: url(img/sc_img1.jpg)">
-                    </div>
-
-                    <div class="product-in-sc-desc">
-                        <h3 class="h3-sc-name">Rebox Zane</h3>
-                        <div class="sc-rating">
-                            <i class="fas fa-star rat"></i>
-                            <i class="fas fa-star rat"></i>
-                            <i class="fas fa-star rat"></i>
-                            <i class="fas fa-star rat"></i>
-                            <i class="fas fa-star rat"></i>
+            <? foreach ($cartData as $cartDataElem): ?>
+                <div class="product-in-sc">
+                    <a href="single-page.htm" style="float: left; width: 240px;">
+                        <div class="product-in-sc-img" style="background-image: url(<?= $cartDataElem['img'] ?>)">
                         </div>
-                        <div class="sc-count">1&nbsp;x $250</div>
 
-                    </div>
-                </a>
-                <div class="sh__action"><a href="#" class="action"><i
-                            class="far fa-times-circle"></i></a></div>
+                        <div class="product-in-sc-desc">
+                            <h3 class="h3-sc-name"><?= $cartDataElem['name'] ?></h3>
+                            <div class="sc-rating">
+                                <? for ($i = 1; $i <= $cartDataElem['rating']; $i++): ?>
+                                <i class="fas fa-star rat"></i>
+                                <? endfor; ?>
+                            </div>
+                            <div class="sc-count"><?= $cartDataElem['qty'] ?>&nbsp;x $<?= $cartDataElem['price'] ?></div>
 
-            </div>
+                        </div>
+                    </a>
+                    <div class="sh__action"><a href="#" class="action"><i
+                                class="far fa-times-circle"></i></a></div>
+
+                </div>
+            <? endforeach; ?>
             <div class="product-in-sc"><a href="single-page.htm" style="float: left; width: 240px;">
                     <div class="product-in-sc-img" style="background-image: url(img/sc_img2.jpg)">
                     </div>
@@ -47,7 +58,7 @@
             </div>
             <div class="total">
                 <div>total</div>
-                <div>$50 0.00</div>
+                <div>$500.00</div>
             </div>
             <a href="checkout.htm" class="sc-btn">Checkout</a>
             <a href="shopping-card.htm" class="sc-btn">go&nbsp;to&nbsp;cart</a>
