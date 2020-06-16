7c52644f69bf60fb9d05dfd272d39385a20f23ee
nikita1984 <52231270+nikita1984@users.noreply.github.com>
2020-06-16 22:30:05 +0300

    tasks 7-1: реализация построения корзины через шаблон (часть_4)

--- les_6/catalog/templates/content_templates/catalog_productCatalog.php
+++ les_6/catalog/templates/content_templates/catalog_productCatalog.php
@@ -1,6 +1,8 @@
 <?php
 $sql = 'SELECT * FROM shopTable';
 $catalogData = SQLQuery($sql, 'All');
+// var_dump($catalogData);
+// var_dump($catalogData[0]);
 closeConnection();
 ?>
 <div class="product-catalog">
@@ -9,7 +11,7 @@ closeConnection();
         $img = getFilePath($item['id'], 'catalog-img');
         ?>
         <div class="catalog-flex">
-            <a href="./index.php?page=singlePage&id=<?= $item['id'] ?>
+                <a href="./index.php?page=singlePage&id=<?= $item['id'] ?>
                                                 &rating=<?= $item['rating'] ?>
                                                 &name=<?= $item['name'] ?>
                                                 &price=<?= $item['price'] ?>
@@ -27,9 +29,10 @@ closeConnection();
                     </div>
                 </div>
             </a>
-            <a href="shopping-card.htm" class="addtocard1">Add to&nbsp;card</a>
+            <button type="submit" value="<?= $item['id'] ?>"><span class="addtocard1">Add to card</span></button>
             <a href="#" class="addtocard2"><i class="far fa-heart"></i></a>
             <a href="#" class="addtocard3"><i class="fas fa-retweet"></i></a>
+
         </div>
     <? endforeach; ?>
 </div>
