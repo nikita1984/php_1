bd75dc7ced41c93d64d8f3805c78b29c61a68375
nikita1984 <52231270+nikita1984@users.noreply.github.com>
2020-06-17 22:56:35 +0300

    tasks 7-1: реализация проброса данных из каталога в корзину (часть 6)

--- les_6/catalog/templates/header_templates/header__right_component.php
+++ les_6/catalog/templates/header_templates/header__right_component.php
@@ -5,7 +5,7 @@ if (array_key_exists('cartData', $_SESSION)) {
     $cartData = $_SESSION['cartData'];
 }
 
-$numbersData = count($cartData);
+$numbersCartPosition = count($cartData);
 
 foreach ($cartData as $item) {
     $total += $item['price'] * $item['qty'];
@@ -14,7 +14,7 @@ foreach ($cartData as $item) {
 <div class="header__right">
     <div class="sh"><a href="shopping-card.htm"><img src="img/Forma_1.svg" alt="basket"
                                                      class="shopping-cart"></a>
-        <div class="sh-count"><?= $numbersData ?></div>
+        <div class="sh-count"><?= $numbersCartPosition ?></div>
         <div class="shopping-cart__drop">
             <? foreach ($cartData as $cartDataElem): ?>
                 <?php
@@ -28,7 +28,7 @@ foreach ($cartData as $item) {
                                                            &description=<?= $cartDataElem['description'] ?>
                                                            &material=<?= $cartDataElem['material'] ?>
                                                            &designer=<?= $cartDataElem['designer'] ?>"
-                                                           style="float: left; width: 240px;">
+                       style="float: left; width: 240px;">
                         <div class="product-in-sc-img" style="background-image: url(<?= $img ?>)">
                         </div>
 
@@ -36,15 +36,16 @@ foreach ($cartData as $item) {
                             <h3 class="h3-sc-name"><?= $cartDataElem['name'] ?></h3>
                             <div class="sc-rating">
                                 <? for ($i = 1; $i <= $cartDataElem['rating']; $i++): ?>
-                                <i class="fas fa-star rat"></i>
+                                    <i class="fas fa-star rat"></i>
                                 <? endfor; ?>
                             </div>
-                            <div class="sc-count"><?= $cartDataElem['qty'] ?>&nbsp;x $<?= $cartDataElem['price'] ?></div>
+                            <div class="sc-count"><?= $cartDataElem['qty'] ?>&nbsp;x
+                                $<?= $cartDataElem['price'] ?></div>
 
                         </div>
                     </a>
                     <div class="sh__action"><a href="#" class="action"><i
-                                class="far fa-times-circle"></i></a></div>
+                                    class="far fa-times-circle"></i></a></div>
 
                 </div>
             <? endforeach; ?>
