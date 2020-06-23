ca199e9f6ffb7230c8c16937424d185a9271be7d
nikita1984 <52231270+nikita1984@users.noreply.github.com>
2020-06-23 11:31:06 +0300

    tasks 7-2: реализация системы верификации пользователя (часть 4)

--- les_6/catalog/public/auth.php
+++ les_6/catalog/public/auth.php
@@ -7,9 +7,9 @@ requireFunctions(scandir(ENGINE_DIR));
 ?>
 <h1>Пожалуйста, авторизуйтесь!!</h1>
 <form action="" method="post">
-    <h3>Введите логин (по умолчанию: user)</h3>
+    <h3>Введите логин (по умолчанию: admin)</h3>
     <input type="text" name="login">
-    <h3>Введите пароль (по умолчанию: user)</h3>
+    <h3>Введите пароль (по умолчанию: admin)</h3>
     <input type="password" name="password">
     <h4>Проверьте введённые данные и нажмите отправить</h4>
     <input type="submit" value="login">
